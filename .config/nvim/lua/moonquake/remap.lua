90a3298f6aa48c93a91f18cf34780bc48074f48b
Jonathan <77164328+qinchonghanzuibang@users.noreply.github.com>
2023-07-20 22:11:35 -0700

    Update remaps

--- .config/nvim/lua/moonquake/remap.lua
+++ .config/nvim/lua/moonquake/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
